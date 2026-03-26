bc34a432a9be1bb222c17f88ed19048973266ec1
Vincent R <vinctlie06@gmail.com>
2026-03-26 14:17:13 +0700

    feat: beautify printing

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"tucil/src/octree"
 	"tucil/src/parser"
 	"tucil/src/point"
@@ -10,8 +11,27 @@ import (
 )
 
 func main() {
+	var filename string
+	var targetSize float64
+
+	// Input
+	fmt.Println("\n=== MAMBA VOXELIZER ===")
+	fmt.Println("Please enter the filename you desire")
+	fmt.Println("Hint : file located in tc/")
+	fmt.Print(">> ")
+	fmt.Scanf("%s\n", &filename)
+
+	fmt.Println("\nEvery voxel is represented with the size of 2 to the power of 'i'")
+	fmt.Println("Please enter your desired 'i'")
+	fmt.Println("Hint : 'i' can be negative")
+	fmt.Println("Hint : 'i' should be adjusted to the file's need")
+	fmt.Print(">> ")
+	fmt.Scanf("%f\n", &targetSize)
+
+	targetSize = math.Pow(2, targetSize)
+
 	// Parse
-	vertices, faces := parser.Parse("tc/bunny.obj")
+	vertices, faces := parser.Parse("tc/" + filename)
 
 	var arrPoints []point.Point
 	arrPoints = point.ToPoint(vertices)
@@ -19,12 +39,12 @@ func main() {
 	var arrFaces []point.Face
 	arrFaces = point.ToFace(arrPoints, faces)
 
-	targetSize := 0.00390625
 
-	fmt.Println("Voxelizing...")
+	fmt.Println("\n=== STARTS VOXELIZING ===")
 
 	rootNode := voxelizer.StartVoxelize(arrPoints, arrFaces, targetSize)
 
+	fmt.Println("\n=== OUTPUT ===")
 	var leaves []*octree.OctreeNode
 	voxelizer.GetLeaves(rootNode, &leaves)
 
@@ -32,7 +52,7 @@ func main() {
 	fmt.Printf("vx : %v\n\n", len(leaves))
 
 	// print vertex & face
-	outputFile := "test/placeholder.obj"
+	outputFile := "test/v_" + filename
 	errr := voxelizer.ExportToOBJ(outputFile, leaves)
 	if errr != nil {
 		log.Fatalf("Failed to save voxels: %v", errr)
