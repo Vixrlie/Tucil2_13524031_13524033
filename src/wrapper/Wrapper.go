cadcb835c3b8ecebb8b4c5523f70fde391e77ce6
Tensai-033 <rayowenmartin2018@gmail.com>
2026-03-27 21:20:45 +0700

    fix: cleaning useless functions and code

--- src/wrapper/Wrapper.go
+++ src/wrapper/Wrapper.go
@@ -6,7 +6,7 @@ import (
 	"tucil/src/point"
 )
 
-func WrapInBox(arrPoint []point.Point) (res []point.Point, oct *octree.OctreeNode) {
+func WrapInBox(arrPoint []point.Point) (oct *octree.OctreeNode) {
 	oct = nil
 	var max_x, max_y, max_z float32 = -math.MaxFloat32, -math.MaxFloat32, -math.MaxFloat32
 
@@ -18,59 +18,58 @@ func WrapInBox(arrPoint []point.Point) (res []point.Point, oct *octree.OctreeNod
 		y := arrPoint[v].Y
 		z := arrPoint[v].Z
 
-		if x > max_x { max_x = x }
-		if x < min_x { min_x = x }
+		if x > max_x {
+			max_x = x
+		}
+		if x < min_x {
+			min_x = x
+		}
 
-		if y > max_y { max_y = y }
-		if y < min_y { min_y = y }
+		if y > max_y {
+			max_y = y
+		}
+		if y < min_y {
+			min_y = y
+		}
 
-		if z > max_z { max_z = z }
-		if z < min_z { min_z = z }
+		if z > max_z {
+			max_z = z
+		}
+		if z < min_z {
+			min_z = z
+		}
 	}
 
 	var sd, cx, cy, cz float32 //s: panjang, c: tengah, sd: stengah panjang maks
 	// ceil biar ga gmpg error
-    cx = float32(math.Ceil(float64((max_x + min_x) / 2)))
-    cy = float32(math.Ceil(float64((max_y + min_y) / 2)))
-    cz = float32(math.Ceil(float64((max_z + min_z) / 2)))
+	cx = float32(math.Ceil(float64((max_x + min_x) / 2)))
+	cy = float32(math.Ceil(float64((max_y + min_y) / 2)))
+	cz = float32(math.Ceil(float64((max_z + min_z) / 2)))
 
 	var maxDist float32 = 0
-    for i := range arrPoint {
-        dx := float32(math.Abs(float64(arrPoint[i].X - cx)))
-        dy := float32(math.Abs(float64(arrPoint[i].Y - cy)))
-        dz := float32(math.Abs(float64(arrPoint[i].Z - cz)))
-        
-        if dx > maxDist { maxDist = dx }
-        if dy > maxDist { maxDist = dy }
-        if dz > maxDist { maxDist = dz }
-    }
-	sd = maxDist
+	for i := range arrPoint {
+		dx := float32(math.Abs(float64(arrPoint[i].X - cx)))
+		dy := float32(math.Abs(float64(arrPoint[i].Y - cy)))
+		dz := float32(math.Abs(float64(arrPoint[i].Z - cz)))
 
+		if dx > maxDist {
+			maxDist = dx
+		}
+		if dy > maxDist {
+			maxDist = dy
+		}
+		if dz > maxDist {
+			maxDist = dz
+		}
+	}
+	sd = maxDist
 
 	// biar kelipatan dua aj
 	var exp float64 = 0
-    for float32(math.Pow(2, exp)) < sd {
-        exp++
-    }
-    sd = float32(math.Pow(2, exp))
+	for float32(math.Pow(2, exp)) < sd {
+		exp++
+	}
+	sd = float32(math.Pow(2, exp))
 
-	min_x = cx - sd/2
-    max_x = cx + sd/2
-    min_y = cy - sd/2
-    max_y = cy + sd/2
-    min_z = cz - sd/2
-    max_z = cz + sd/2
-
-	res = []point.Point{}
-
-	res = append(res, point.Point{X: min_x, Y: min_y, Z: min_z},
-		point.Point{X: min_x, Y: min_y, Z: max_z},
-		point.Point{X: min_x, Y: max_y, Z: min_z},
-		point.Point{X: min_x, Y: max_y, Z: max_z},
-		point.Point{X: max_x, Y: min_y, Z: min_z},
-		point.Point{X: max_x, Y: min_y, Z: max_z},
-		point.Point{X: max_x, Y: max_y, Z: min_z},
-		point.Point{X: max_x, Y: max_y, Z: max_z})
-
-	return res, octree.NewOctreeNode(point.Point{X: cx, Y: cy, Z: cz}, sd)
+	return octree.NewOctreeNode(point.Point{X: cx, Y: cy, Z: cz}, sd)
 }
